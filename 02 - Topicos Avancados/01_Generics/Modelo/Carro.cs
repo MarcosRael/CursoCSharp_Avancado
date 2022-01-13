c465def376e4f49cb6b3edec3a821839de5597ec
MarcosRael <marcosasrael@gmail.com>
2022-01-13 19:43:19 -0300

    chore: Deserializando com o uso de Generics

---
+++ 02 - Topicos Avancados/01_Generics/Modelo/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Generics.Modelo
+{
+    class Carro
+    {
+
+        public string Modelo { get; set; }
+
+        public string Marca { get; set; }
+    }
+}
---
+++ 02 - Topicos Avancados/01_Generics/Serializador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace _01_Generics
+{
+    class Serializador
+    {
+
+        private const string diretorio =
+               @"C:\Projetos\Treinamentos\Cursos\CursoCSharp_Avancado\Diretorio\";
+
+        public static void Serializar(object obj)
+        {
+            StreamWriter sw = new StreamWriter(diretorio + obj.GetType().Name + ".json");
+
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            string objSerializado = serializador.Serialize(obj);
+
+            sw.Write(objSerializado);
+            sw.Close();
+
+        }
+
+        public static T Deserializar<T>()
+        {
+            StreamReader sr = new StreamReader(diretorio + typeof(T).Name + ".json");
+
+            string conteudo = sr.ReadToEnd();
+
+            JavaScriptSerializer deselizador = new JavaScriptSerializer();
+
+            return (T) deselizador.Deserialize(conteudo, typeof(T));
+
+        }
+
+    }
+}
