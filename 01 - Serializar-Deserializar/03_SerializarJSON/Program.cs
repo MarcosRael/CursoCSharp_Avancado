018f0cba06ae59f5a743faff7a1d285df8cbd843
MarcosRael <marcosasrael@gmail.com>
2022-01-13 07:54:13 -0300

    chore: Serializar e Deserializar um arquivos JSON

---
+++ 01 - Serializar-Deserializar/03_SerializarJSON/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using _00_Biblioteca;
+using System.IO;
+
+namespace _03_SerializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretorioJSON = @"C:\Projetos\Treinamentos\Cursos\CursoCSharp_Avancado\Diretorio\03_SerializarJSON.json";
+
+            Usuario usuario = new Usuario()
+            {
+                Nome = "Maria",
+                CPF = "333.333.333-33",
+                Email = "maria@hotmail.com"
+            };
+
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            string stringObjSerializador = serializador.Serialize(usuario);
+
+            StreamWriter sw = new StreamWriter(diretorioJSON);
+            
+            sw.WriteLine(stringObjSerializador);
+            sw.Close();
+
+        }
+    }
+}
---
+++ 01 - Serializar-Deserializar/04_DeserializarJSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+using _00_Biblioteca;
+
+namespace _04_DeserializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretorioJSON = @"C:\Projetos\Treinamentos\Cursos\CursoCSharp_Avancado\Diretorio\03_SerializarJSON.json";
+
+            StreamReader stream = new StreamReader(diretorioJSON);
+            string linhasDoArquivo = stream.ReadToEnd();
+
+            JavaScriptSerializer deserializador = new JavaScriptSerializer();
+            Usuario usuario = (Usuario) deserializador.Deserialize(linhasDoArquivo, typeof(Usuario));
+
+            Console.WriteLine(
+                "Usuario(Nome): {0}, CPF: {1}, Email: {2}",
+                usuario.Nome, usuario.CPF, usuario.Email);
+
+            Console.ReadKey();
+
+        }
+    }
+}
