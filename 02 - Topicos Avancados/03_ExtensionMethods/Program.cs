e9c0614bb8e04f3fefeacded34d3a12f1779590d
MarcosRael <marcosasrael@gmail.com>
2022-01-13 23:01:17 -0300

    chore: Criação de novos metodos no objeto String

---
+++ 02 - Topicos Avancados/03_ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor = Console.ReadLine();
+
+            Console.WriteLine(valor.FirstToupper());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02 - Topicos Avancados/03_ExtensionMethods/StringExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ExtensionMethods //É possivel colocar no namespace System para não precisar importar sempre
+{
+    public static class StringExtension //: String  -> Não pode ser herdada pq é uma classe Sealed
+    {
+
+        public static string FirstToupper(this String str)
+        {
+            string[] palavras = str.Split(' ');
+            string res = string.Empty;
+
+            foreach (var pv in palavras)
+            {
+                string p1 = pv.Substring(0, 1).ToUpper();
+                string p2 = pv.Substring(1).ToLower();
+
+                res += p1 + p2 + " ";
+            }
+            
+            return res;
+        }
+
+    }
+}
