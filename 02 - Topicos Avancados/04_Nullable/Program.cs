e4171b0c3f6e39c2d246b6fadfa1151587bcfbc4
MarcosRael <marcosasrael@gmail.com>
2022-01-13 23:12:44 -0300

    chore: Fundamentos sobre Nullable

---
+++ 02 - Topicos Avancados/04_Nullable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Não aceita null - por valor
+            //ERRO: int i = null;
+
+            //Class - por referencia
+            object o = null;
+
+            //MySQL - Oracle - PostGreeSQL .... NoSQL - Informações opcionais
+            //ERRO: byte = null;
+            Nullable<byte> IdadeA = null;
+            byte? IdadeB = null; //Também pode ser representado dessa maneira
+
+        }
+    }
+}
