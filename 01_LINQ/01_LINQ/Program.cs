62d1e2bda07f806b6baecc211972572e3448431a
MarcosRael <marcosasrael@gmail.com>
2022-01-15 12:05:36 -0300

    chore: LINQ e LAMBDA

---
+++ 01_LINQ/01_LINQ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //  LINQ E LAMBDA
+            // LAMBDA - (ENTRADA => (EXPRESSÃO)
+
+            int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 };
+
+            var listaFilrtada = lista.Where(a => a > 6).OrderByDescending(a => a).Select(a => a);
+
+            int listaCount = lista.Count(a => a > 6);
+
+            foreach(var index in listaFilrtada)
+                Console.WriteLine(index);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 01_LINQ/02_LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  LINQ E LAMBDA
+            // LAMBDA - (ENTRADA => (EXPRESSÃO)
+
+            int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 };
+
+            //var listaFilrtada = lista.Where(a => a > 6).OrderByDescending(a => a).Select(a => a);
+
+            var listaFilrtada = from a in lista where a > 10 orderby a select a;
+
+            foreach (var index in listaFilrtada)
+                Console.WriteLine(index);
+
+            Console.ReadKey();
+
+        }
+    }
+}
