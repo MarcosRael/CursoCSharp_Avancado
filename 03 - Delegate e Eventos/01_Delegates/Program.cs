6bb3c96dac8bbb7805039bdf44d9595e57aa12d7
MarcosRael <marcosasrael@gmail.com>
2022-01-19 21:39:09 -0300

    chore: Utilização de Delegates Parte 1, 2 e 3.

---
+++ 03 - Delegate e Eventos/01_Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Delegates
+{
+    class Program
+    {
+        delegate int Calcula(int a, int b);
+
+        static void Main(string[] args)
+        {
+            //var so = Soma(10, 20);
+            //var su = Subtrair(20, 10);
+
+            Calcula calc1 = new Calcula(Soma);
+            var so = calc1(10, 20);
+
+            Calcula calc2 = new Calcula(Subtrair);
+            var su = calc2(20, 10);
+
+            Console.WriteLine($"Soma: {so}, Sub: {su}");
+            Console.ReadKey();
+
+        }
+
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ 03 - Delegate e Eventos/02_Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02_Delegates.Lib;
+
+namespace _02_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Tela - Cadastro de Usuario: Thumb (Foto de Perfil)
+            Foto foto1 = new Foto() { Nome = "perfil.jpg", TamanhoX = 1920, TamanhoY = 1080 };
+            FotoProcessador.filtros = new FotoFiltro().GerarThumb;
+            FotoProcessador.Processador(foto1);
+
+            //Tela - Cadastro de Produtos: Colorir + TamanhoMed
+            Foto foto2 = new Foto() { Nome = "produto.jpg", TamanhoX = 1920, TamanhoY = 1080 };
+            FotoProcessador.filtros = new FotoFiltro().Colorir;
+            FotoProcessador.filtros += new FotoFiltro().RedimensionarTamMedio;
+            FotoProcessador.Processador(foto2);
+
+            //Tela - Cadastro de Albuns do Usuario - Retro: Preto e Branco
+            Foto foto3 = new Foto() { Nome = "Album.jpg", TamanhoX = 1920, TamanhoY = 1080 };
+            FotoProcessador.filtros = new FotoFiltro().PretoBranco;
+            FotoProcessador.Processador(foto3);
+
+            Console.ReadKey();
+        }
+    }
+}
