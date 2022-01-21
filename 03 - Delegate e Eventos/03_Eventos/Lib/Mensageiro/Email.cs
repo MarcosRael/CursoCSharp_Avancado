30f3232798e90ef7d0f97beed106af3100cb42cc
MarcosRael <marcosasrael@gmail.com>
2022-01-20 21:46:07 -0300

    chore: Utilização de Eventos parte 1, 2 e 3.

---
+++ 03 - Delegate e Eventos/03_Eventos/Lib/Mensageiro/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Eventos.Lib.Mensageiro
+{
+    public class Email
+    {
+        public void EnviarMensagem(object sender, VideoEventArgs args)
+        {
+            Console.WriteLine($"Email Enviado para o video {args.Video.Nome}");
+        }
+
+    }
+}
---
+++ 03 - Delegate e Eventos/03_Eventos/Lib/VideoEncode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _03_Eventos.Lib
+{
+    public class VideoEncode
+    {
+
+        //public delegate void VideoEncodeHandler(Videos video);
+        //public event VideoEncodeHandler Encoded;
+
+        public event EventHandler<VideoEventArgs> Encoded;
+        
+        public void Encode(Videos video)
+        {
+            Console.WriteLine("Convertendo o  video....");
+            Thread.Sleep(2000);
+            Console.WriteLine("Video convertido!");
+
+            Encoded(this, new VideoEventArgs() { Video = video });
+
+        }
+    }
+    public class VideoEventArgs : EventArgs
+    {
+        public Videos Video { get; set; }
+    }
+}
