58ae4a5b32d90a1cf5ab2ea1cf0828d09e1706df
Marcos Rael <marcosasrael@gmail.com>
2022-01-29 19:20:43 -0300

    chore: Parametros pelo metodo Start na thread.

---
+++ 01_Thread/06_ThreadId/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _06_ThreadId
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.Start(i);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao(object Id)//Vai receber o parametro pelo metodo Start da thread
+        {
+            for (int i = 0; i < 1000; i++)
+                Console.WriteLine($"Thread: {Id} - Num: {i} - Thread Id Nativo: {Thread.CurrentThread.ManagedThreadId}");
+
+        }
+    }
+}
+
