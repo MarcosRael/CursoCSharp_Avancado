20a6f5065f21414375f696f07b26f6d2594b4596
Marcos Rael <marcosasrael@gmail.com>
2022-02-01 07:44:04 -0300

    chore: Task em metodos.

---
+++ 01_Task/02_TaskExecute/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_TaskExecute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 01
+            Task.Run(() => Metodo01());
+            //Task 02
+            Task.Run(() => Metodo01());
+            //Task 03 (Outra forma para inicializar)
+            Task.Factory.StartNew(Metodo01);
+
+            Console.ReadKey();
+        }
+
+
+
+        static void Metodo01()
+        {
+            for (int i = 0; i < 1000; i++)
+                Console.WriteLine($"Valor: {i}, ID Task: {Task.CurrentId}");
+        }
+    }
+}
